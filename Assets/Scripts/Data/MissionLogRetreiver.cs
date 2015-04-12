4fa026f9b9dcaf1cf313b132d882a8b9190d786a
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-04-12 21:43:41 +0100

    MissionLogs now work from set file

--- Assets/Scripts/Data/MissionLogRetreiver.cs
+++ Assets/Scripts/Data/MissionLogRetreiver.cs
@@ -8,22 +8,28 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public class MissionLogRetreiver : MonoBehaviour {
 
+    void Start()
+    {
+        ReadData("MissionName.dat");
+    }
+
     public Text Log;
 
     void ReadData(string FileName)
     {
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream fs = File.Create(Application.persistentDataPath + @"\" + FileName);
+        FileStream fs = File.Open(Application.persistentDataPath + @"\" + FileName, FileMode.Open);
         DataList data = bf.Deserialize(fs) as DataList;
 
         string ObtainedDataMessage = "";
 
         foreach (PlanetData pd in data.Data)
         {
-            ObtainedDataMessage += string.Format("{0}: {1}", pd.name, pd.data);
+            ObtainedDataMessage += string.Format("{0}: {1}\n", pd.name, pd.data);
         }
 
-        Log.text += string.Format("{0}: {1}", FileName, ObtainedDataMessage);
+        Log.text += string.Format("{0}:\n {1}", FileName, ObtainedDataMessage);
+        Debug.Log(string.Format("{0}: {1}", FileName, ObtainedDataMessage));
 
         fs.Close();
     }
